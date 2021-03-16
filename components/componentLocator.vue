bef372aa8e25a5fe11bb9c28494d9e9062effc66
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2021-03-16 22:57:18 +0200

    locator button desktop view

--- components/componentLocator.vue
+++ components/componentLocator.vue
@@ -27,6 +27,9 @@ export default {
 #newLocation {
   @apply absolute z-50 right-0 w-12 h-12 rounded-full bg-white my-3 mx-3 flex justify-center items-center;
   box-shadow: 0 0px 0px 7px rgb(180 194 213 / 5%);
+  @screen lg {
+    right: 400px;
+  }
   i {
     font-size: 24px;
     @apply text-primary;
