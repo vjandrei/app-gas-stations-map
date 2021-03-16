a8941f5c432411ae4ff5d9690ba13d62614d20cb
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2021-03-16 22:53:25 +0200

    ui changes

--- components/componentMap.vue
+++ components/componentMap.vue
@@ -73,7 +73,17 @@ export default {
     }
   },
   mounted() {
-    this.$nuxt.$on('select-station', (station) => {})
+    this.$nuxt.$on('select-station', (station) => {
+      this.$nextTick(() => {
+        this.$refs.map.mapObject.fitBounds(
+          [[station.coords.lat, station.coords.lng]],
+          {
+            paddingBottomRight: [0, 200],
+            maxZoom: 16,
+          }
+        )
+      })
+    })
 
     this.$store
       .dispatch('GET_USER_SESSION_LOCATION_DATA', this.userCoords)
--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,42 @@
 <template>
-  <div>
+  <div class="container mx-auto">
     <div id="welcomeScreen">
       <div id="welcomeScreenContainer">
         <div>
-          <h1 class="text-center">Löydä kaasutankkausasemat ympäri Suomessa</h1>
-          <h2 class="text-center">
-          Kaasulla.app on karttapohjainen sovellus josta löydät kaasutankkausasemat joissa voi tankkaa biokaasua tai maakaasua. Sinun tarvitsee vain myötään sovellukselle paikannus oikeus.
+          <h1>Löydä kaasutankkausasemat ympäri Suomessa</h1>
+          <h2>
+            Kaasulla.app on karttapohjainen sovellus josta löydät
+            kaasutankkausasemat joissa voi tankkaa biokaasua tai maakaasua.
+            Sinun tarvitsee vain myötään sovellukselle paikannus oikeus.
           </h2>
         </div>
         <div>
-         <ul id="appFeaturesList">
-           <li class="appFeaturesListItem">
-             <div class="featuresHeading">
-               <h3>Paikannus</h3>
-               <h4>Alueesi lähimmät kaasutankkausasemat</h4>
-             </div>
-           </li>
-           <li class="appFeaturesListItem">
-             <div class="featuresHeading">
-               <h3>Suodattaminen</h3>
-               <h4>Rajaa kaasutankkausasemia kaasutyypin mukaan</h4>
-             </div>
-           </li>
-           <li class="appFeaturesListItem">
-             <div class="featuresHeading">
-               <h3>Navigointi</h3>
-               <h4>Reittiohje navigointi applikaatioon</h4>
-             </div>
-           </li>
-         </ul>
+          <ul id="appFeaturesList">
+            <li class="appFeaturesListItem">
+              <div class="featuresHeading">
+                <h3>Paikannus</h3>
+                <h4>Alueesi lähimmät kaasutankkausasemat</h4>
+              </div>
+            </li>
+            <li class="appFeaturesListItem">
+              <div class="featuresHeading">
+                <h3>Suodattaminen</h3>
+                <h4>Rajaa kaasutankkausasemia kaasutyypin mukaan</h4>
+              </div>
+            </li>
+            <li class="appFeaturesListItem">
+              <div class="featuresHeading">
+                <h3>Navigointi</h3>
+                <h4>Reittiohje navigointi applikaatioon</h4>
+              </div>
+            </li>
+          </ul>
         </div>
         <div>
           <button class="largeButton" @click="getUserLocation">
-            <span v-if="userLocation"
-              >Haetaan
-              paikkatietoja</span
-            >
+            <span v-if="userLocation">Haetaan paikkatietoja</span>
             <span v-else>Paikanna minut</span>
           </button>
-
         </div>
       </div>
     </div>
@@ -72,16 +70,15 @@ export default {
 */
 
 #welcomeScreen {
-  @apply relative  bg-local bg-center bg-no-repeat bg-cover flex flex-col h-full text-white justify-end;
+  @apply relative  bg-local bg-center bg-no-repeat bg-auto flex flex-col h-full text-white justify-end;
   &::before {
     content: '';
     @apply absolute inset-0 w-full h-full bg-no-repeat bg-contain bg-top;
     //filter: grayscale(60%);
-    background-image: url('~assets/img/illustrator-bg.svg');
+    //background-image: url('~assets/img/illustrator-bg.svg');
   }
   @apply relative flex flex-col h-full text-white justify-end bg-white;
 }
-
 
 #welcomeScreenContainer {
   @apply font-display p-4 pb-8 m-auto relative w-full max-w-lg text-secondary;
@@ -99,20 +96,20 @@ export default {
   }
 }
 
-#appFeaturesList{
+#appFeaturesList {
   @apply my-8;
 }
-.appFeaturesListItem{
+.appFeaturesListItem {
   @apply flex my-4 content-center items-center;
 }
-.featuresIcon{
+.featuresIcon {
   @apply w-12 mr-4 flex justify-center items-start;
 }
-.featuresHeading{
-  h3{
-    @apply text-lg text-secondary; 
+.featuresHeading {
+  h3 {
+    @apply text-lg font-semibold text-secondary;
   }
-  h4{
+  h4 {
     @apply text-xs text-secondary;
   }
 }
